124aad98e5a5ca38b415679bfac166da42194837
rodsn <rodrigosousaneves@live.com>
2017-10-29 17:56:50 +0000

    added udp

--- states/game.lua
+++ states/game.lua
@@ -1,6 +1,12 @@
 game = {}
 
 function game.load(  )
+	local socket = require "socket"
+	local address, port = "25.95.217.205", 13243
+
+	udp = socket.udp()
+	udp:settimeout(0)
+	udp:setpeername(address, port)
 end
 
 function game.update( dt )
@@ -8,11 +14,11 @@ end
 
 function game.draw(  )
 	debug.print("Fps", love.timer.getFPS())
-	love.graphics.print("Ola bife. O mike gregou", 20, 20)
+	
 end
 
 function game.keypressed( key )
-
+	udp:send(key)
 end
 
 function game.mousepressed( x, y, btn )
